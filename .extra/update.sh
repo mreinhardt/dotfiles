151a4a13b4776e7cc6292b2450dc31f7e4d26305
Mike Reinhardt <mreinhardt@gmail.com>
2016-10-30 12:54:45 -0700

    Change repo update to hard reset

--- .extra/update.sh
+++ .extra/update.sh
@@ -1,8 +1,11 @@
 #!/bin/bash -e
 
+git_cmd () {
+    $(which git) --work-tree="$DOTFILES_REPO_DIR" --git-dir="$DOTFILES_REPO_DIR/.git" $@
+}
+
 # Update dotfiles repo itself.
 if [ -d "$DOTFILES_REPO_DIR/.git" ]; then
-    git --work-tree="$DOTFILES_REPO_DIR" \
-        --git-dir="$DOTFILES_REPO_DIR/.git" \
-        pull origin master
+    git_cmd fetch origin
+    git_cmd reset --hard origin/master
 fi
