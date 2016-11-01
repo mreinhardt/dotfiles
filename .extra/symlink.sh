a089843511f72707be5456fd48fc463632a81aed
Mike Reinhardt <mreinhardt@gmail.com>
2016-11-01 00:30:24 -0700

    Fix install symlinks

--- .extra/symlink.sh
+++ .extra/symlink.sh
@@ -3,20 +3,23 @@
 
 ### zsh
 
-ln -sfv "$DOTFILES_REPO_DIR/.important_commands" ~
-ln -sfv "$DOTFILES_REPO_DIR/.zshenv" ~
+ln -sfv "$DOTFILES_REPO_DIR/.important_commands" "$HOME"
+ln -sfv "$DOTFILES_REPO_DIR/.zshenv" "$HOME"
 
 # Can't symlink .zsh directly since files like .zsh_history are added dynamically
-mkdir -p ~/.zsh
-for zsh_source in $(find $DOTFILES_REPO_DIR/.zsh -maxdepth 1 | tail -n+2); do
+mkdir -p "$HOME/.zsh"
+for zsh_source in $(find "$DOTFILES_REPO_DIR/.zsh" -maxdepth 1 | tail -n+2); do
     if [[ ! -d "$zsh_source" ]]; then
-        ln -sfv "$zsh_source" ~/.zsh
+        ln -sfv "$zsh_source" "$HOME/.zsh"
     fi
 done
+
+mkdir -p "$HOME/.zsh/.zfunctions"
+ln -sfv "$DOTFILES_REPO_DIR/.zsh/.prompts/sfz/prompt_sfz_setup" "$HOME/.zsh/.zfunctions"
 
 
 ### tmux
 
-ln -sfv "$DOTFILES_REPO_DIR/.tmux.conf" ~
-ln -sfv "$DOTFILES_REPO_DIR/.tmux" ~
+ln -sfv "$DOTFILES_REPO_DIR/.tmux.conf" "$HOME"
+ln -sfv "$DOTFILES_REPO_DIR/.tmux" "$HOME"
 
