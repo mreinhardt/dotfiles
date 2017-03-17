47f5b21155bbcecc961b9d0d4d28a54d9b46437e
Michael Reinhardt <mreinhardt@gmail.com>
2017-03-17 16:48:21 -0700

    Force weather in F

--- .tmux/status-weather.sh
+++ .tmux/status-weather.sh
@@ -6,7 +6,7 @@ function saveweather {
     local full_weather
     local raw_weather
     local weather
-    full_weather=$(curl -s "http://wttr.in/$(egrep -o '[^/]*$' < /etc/timezone)")
+    full_weather=$(curl -s "http://wttr.in/$(egrep -o '[^/]*$' < /etc/timezone)?u")
     raw_weather=$(echo "$full_weather" | head -n4 | tail -n2 | tr -d '[:cntrl:]' | sed 's/\[[^m]*m//g')
     weather=$(echo "$raw_weather" | egrep -o '[A-Z][A-Za-z]+(\s*[a-z]+)*|[0-9]+ °F' | tr '\n' ' ' | sed -e 's/ °/°/' -e 's/ *$//g')
     echo -n "$weather" > "$TMP_FILE"
