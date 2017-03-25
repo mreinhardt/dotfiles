1a7001cfe99a0e79c315fe9b26a6ef8d480712b1
Michael Reinhardt <mreinhardt@gmail.com>
2017-03-24 23:36:47 -0700

    Change install copy options

--- .extra/copy.sh
+++ .extra/copy.sh
@@ -5,20 +5,20 @@ cp -vn "$DOTFILES_REPO_DIR/.important_commands" "$HOME"
 
 ### conky
 mkdir -p "$HOME/.conky"
-cp -vn "$DOTFILES_REPO_DIR/.conkyrc" "$HOME"
+cp -vi "$DOTFILES_REPO_DIR/.conkyrc" "$HOME"
 
 
 ### git
 
-cp -vn "$DOTFILES_REPO_DIR/.gitconfig" "$HOME"
+cp -vu "$DOTFILES_REPO_DIR/.gitconfig" "$HOME"
 
 
 ### lein
 
 mkdir -p "$HOME/.lein"
-cp -vn "$DOTFILES_REPO_DIR/.lein/profiles.clj" "$HOME/.lein"
+cp -vu "$DOTFILES_REPO_DIR/.lein/profiles.clj" "$HOME/.lein"
 
 
 ### xmonad
 
-cp -vn "$DOTFILES_REPO_DIR/.xsessionrc" "$HOME"
+cp -vi "$DOTFILES_REPO_DIR/.xsessionrc" "$HOME"
